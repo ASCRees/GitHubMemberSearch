b3ae38c31a2034761105a72051c835612af84aae
arees2020 <andrew.rees2020@gmail.com>
2020-01-14 21:35:37 +0000

    feature/GitSearch-15 - Added some extra validation using javascript and added jasmine tests using jasmine and chutzpah to enable testing within VS Studio

---
+++ GitHubMemberSearch/Scripts/Tests/validate.input.tests.js
@@ -0,0 +1,16 @@
+﻿// Make sure that your javscript code doesn't use any ES6 funcitonality such as let of const or arrow functions
+// Otherwise Chutpah wont recognise the tests and show them in the test explorer.
+// Make sure that the chutzpah.json file is set up and points to the correct source and test directory
+
+describe('JS Test Validate Input',
+    function () {
+        it('Check for string length of 10', function () {
+            var result = countValidCharacters("abcdefghij");
+            expect(result).toEqual(true);
+        });
+
+        it('Check for string length of 40', function () {
+            var result = countValidCharacters("abcdefghijabcdefghijabcdefghijabcdefghij");
+            expect(result).toEqual(false);
+        });
+    });
